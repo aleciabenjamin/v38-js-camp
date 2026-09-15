7d53850b1cc6fae1679f8594d90e00f3036775a9
aleciabenjamin <aleciabenjamin@gmail.com>
2026-09-15 12:03:39 +0200

    feat: introduce const and basic types

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-console.log("Nord Glow");
-console.log("Nord Glow");
-console.log("Nord Glow");
+const eventName = "Primavera Sound";
+let ticketCount = 0;
+
+console.log(eventName);
+console.log(eventName);
+ticketCount = 10;
+console.log(ticketCount);
+const isVip = false;
